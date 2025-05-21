a04e44a24a8289100b1b2940818be80ad8f4009e
aoshide <bargybaev312@gmail.com>
2025-05-22 03:32:50 +0600

    ыфф

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      "whsec_2HZyYgB0invmL16kupzlbcYtSajL2d/6"
     );
   }
 
